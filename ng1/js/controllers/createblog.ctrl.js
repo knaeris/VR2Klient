3ce347672ebace42e1f34954033755c0267b16c8
Indro Kottise <ikottise@itcollege.ee>
2018-05-17 16:06:11 +0300

    Blog view mumbojumbo

--- ng1/js/controllers/createblog.ctrl.js
+++ ng1/js/controllers/createblog.ctrl.js
@@ -3,15 +3,18 @@
 
     angular.module('app').controller('CreateBlogController', Ctrl);
 
-    function Ctrl($http){
+    function Ctrl($http, $location){
 
         var vm = this;
-        var urlCat = 'https://localhost:44305/api/blogcategories';
-        var urlBlogs = 'https://localhost:44305/api/blogs/';
+        var urlCat = 'https://localhost:44305/api/blogcategories/';
+        var urlBlogs = 'https://localhost:44305/api/blogs';
+        var urlBlogPosts = 'https://localhost:44305/api/blogposts';
         this.categories = [];
+        vm.blogPost = {};
+        vm.blog = {};
+        vm.submitData = submitData;
 
         initCat();
-        initBlogTitle();
 
         function initCat() {
             $http.get(urlCat).then(function (result) {
@@ -20,11 +23,10 @@
             });
         }
 
-        function initBlogTitle() {
-            $http.get(urlBlogs).then(function (result) {
-                vm.categories = result.data;
-                console.log(vm.categories);
-            });
+        function submitData() {
+            $http.post(urlBlogPosts, vm.blogPost)
+                //.then($location.path('/main'))
+
         }
 
     }
